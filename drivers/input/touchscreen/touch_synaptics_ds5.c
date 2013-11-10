0658e5340622bcb2ace855f1d7becbe2b030208a
Dennis Rassmann <showp1984@gmail.com>
2013-11-10 01:19:58 +0100

    Revert "sweep2wake: add irq_wake indicator"
    This reverts commit 95c2743e3b0d363df39c4971de4e367f93b1eaf1.

--- drivers/input/touchscreen/touch_synaptics_ds5.c
+++ drivers/input/touchscreen/touch_synaptics_ds5.c
@@ -516,12 +516,6 @@ static void touch_init_func(struct work_struct *work_init)
 	if (!ts->curr_resume_state) {
 		enable_irq(ts->client->irq);
 		mutex_unlock(&ts->input_dev->mutex);
-#ifdef CONFIG_TOUCHSCREEN_SWEEP2WAKE
-		if (irq_wake) {
-			irq_wake = false;
-			disable_irq_wake(ts->client->irq);
-		}
-#endif
 		return;
 	}
 
@@ -530,10 +524,7 @@ static void touch_init_func(struct work_struct *work_init)
 	enable_irq(ts->client->irq);
 
 #ifdef CONFIG_TOUCHSCREEN_SWEEP2WAKE
-	if (irq_wake) {
-		irq_wake = false;
-		disable_irq_wake(ts->client->irq);
-	}
+	enable_irq_wake(ts->client->irq);
 #endif
 
 	mutex_unlock(&ts->input_dev->mutex);
@@ -551,21 +542,9 @@ static void touch_recover_func(struct work_struct *work_recover)
 				struct synaptics_ts_data, work_recover);
 
 	disable_irq(ts->client->irq);
-#ifdef CONFIG_TOUCHSCREEN_SWEEP2WAKE
-	if (irq_wake) {
-		irq_wake = false;
-		disable_irq_wake(ts->client->irq);
-	}
-#endif
 	safety_reset(ts);
 	touch_ic_init(ts);
 	enable_irq(ts->client->irq);
-#ifdef CONFIG_TOUCHSCREEN_SWEEP2WAKE
-	if (!irq_wake) {
-		irq_wake = false;
-		enable_irq_wake(ts->client->irq);
-	}
-#endif
 }
 
 /* touch_ic_init
@@ -594,12 +573,6 @@ static int touch_ic_init(struct synaptics_ts_data *ts)
 err_out_retry:
 	ts->ic_init_err_cnt++;
 	disable_irq_nosync(ts->client->irq);
-#ifdef CONFIG_TOUCHSCREEN_SWEEP2WAKE
-	if (irq_wake) {
-		irq_wake = false;
-		disable_irq_wake(ts->client->irq);
-	}
-#endif
 	safety_reset(ts);
 	queue_delayed_work(synaptics_wq, &ts->work_init, msecs_to_jiffies(10));
 
@@ -1446,12 +1419,6 @@ static ssize_t store_ts_reset(struct device *dev,
 	sscanf(buf, "%s", string);
 
 	disable_irq_nosync(ts->client->irq);
-#ifdef CONFIG_TOUCHSCREEN_SWEEP2WAKE
-	if (irq_wake) {
-		irq_wake = false;
-		disable_irq_wake(ts->client->irq);
-	}
-#endif
 
 	cancel_delayed_work_sync(&ts->work_init);
 
@@ -1489,12 +1456,6 @@ static ssize_t store_ts_reset(struct device *dev,
 		touch_ic_init(ts);
 
 	enable_irq(ts->client->irq);
-#ifdef CONFIG_TOUCHSCREEN_SWEEP2WAKE
-	if (!irq_wake) {
-		irq_wake = false;
-		enable_irq_wake(ts->client->irq);
-	}
-#endif
 
 	return count;
 }
@@ -1777,10 +1738,6 @@ static int lcd_notifier_callback(struct notifier_block *this,
 		}
 #ifdef CONFIG_TOUCHSCREEN_SWEEP2WAKE
 		scr_suspended = true;
-		if (!irq_wake) {
-			irq_wake = true;
-			enable_irq_wake(ts->client->irq);
-		}
 #endif
 		break;
 	default:
