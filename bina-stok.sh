463e1b779f994d5d71e1b573951fe9b375829933
Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>
2014-08-12 19:31:46 +0800

    edit compiler for build
    Signed-off-by: Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>

--- bina-stok.sh
+++ bina-stok.sh
@@ -3,13 +3,14 @@ export ARCH=arm
 #export CROSS_COMPILE=/opt/gcc-4.8-linaro/bin/arm-cortex_a15-linux-gnueabi- 
 #export CROSS_COMPILE=/opt/gcc-4.7-linaro/bin/arm-cortex_a15-linux-gnueabi- 
 #export CROSS_COMPILE=/opt/sabermod-gcc/bin/arm-eabi-
-export CROSS_COMPILE=/opt/gcc-linaro/arm-cortex_a15-linux-gnueabihf-linaro_4.9.1-2014.07/bin/arm-cortex_a15-linux-gnueabihf-
+#export CROSS_COMPILE=/opt/gcc-linaro/arm-cortex_a15-linux-gnueabihf-linaro_4.9.1-2014.07/bin/arm-cortex_a15-linux-gnueabihf-
+export CROSS_COMPILE=/opt/old-linaro/arm-linux-gnueabi-linaro_4.7.4-2014.01/bin/arm-linux-gnueabi-
 #echo "Cleaning old craps..."
 #make distclean
 
 export KBUILD_BUILD_USER=najmi
 export KBUILD_BUILD_HOST="ampang"
-export LOCALVERSION="-v1.3"
+export LOCALVERSION="-v1.4"
 
 #echo "Copy backup config..."
 #cp najmi-mako-config .config
